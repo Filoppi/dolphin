ad80fda53db979c649a604c977dfd90f9390445f
Filoppi <filippotarpini@hotmail.it>
2021-03-06 20:00:47 +0200

    Qt: reverted unnecessary and wrong change

--- Source/Core/DolphinQt/Config/WiimoteControllersWidget.cpp
+++ Source/Core/DolphinQt/Config/WiimoteControllersWidget.cpp
@@ -314,8 +314,7 @@ void WiimoteControllersWidget::LoadSettings()
   else
     m_wiimote_emu->setChecked(true);
 
-  // Make sure continuous scanning setting is applied.
-  WiimoteReal::Initialize(::Wiimote::InitializeMode::DO_NOT_WAIT_FOR_WIIMOTES);
+  UpdateDisabledWiimoteControls();
 }
 
 void WiimoteControllersWidget::SaveSettings()
