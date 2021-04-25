31830d46ff6e996cb132d57f16aefd09bbb781aa
Filoppi <filippotarpini@hotmail.it>
2021-04-26 00:51:48 +0300

    Qt: cleaning ToolTip code

--- Source/Core/DolphinQt/Config/ToolTipControls/ToolTipWidget.h
+++ Source/Core/DolphinQt/Config/ToolTipControls/ToolTipWidget.h
@@ -10,6 +10,8 @@
 
 #include "DolphinQt/Config/Graphics/BalloonTip.h"
 
+constexpr int TOOLTIP_DELAY = 300;
+
 template <class Derived>
 class ToolTipWidget : public Derived
 {
@@ -25,7 +27,7 @@ private:
   {
     if (m_timer_id)
       return;
-    m_timer_id = this->startTimer(300);
+    m_timer_id = this->startTimer(TOOLTIP_DELAY);
   }
 
   void leaveEvent(QEvent* event) override
@@ -49,6 +51,7 @@ private:
 
   virtual QPoint GetToolTipPosition() const = 0;
 
+protected:
   std::optional<int> m_timer_id;
   QString m_title;
   QString m_description;
